5cf49c6b3e6250457061ece1e99115462d5ec83f
Mahesh Shakya <maheshshakya.01@gmail.com>
2020-06-09 21:23:17 +0545

    linear curve fitting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # first-course-in-machine-learning
 source code for first course in machine learning, Simon Rogers, Mark Girolami.
+sample data from: http://www.dcs.gla.ac.uk/~srogers/firstcourseml/
---
+++ fitlinear.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+
+
+def read_olympics_data():
+    filename = './data/olympics.mat'
+    mat = scipy.io.loadmat(filename)
+    print(mat['male100'].shape)
+    print(mat['male100'])
+
+    return mat['male100']
+
+
+def plot_data(X, t, Xnew, tnew):
+    plt.plot(X, t, label='Original')
+    plt.plot(Xnew, tnew, label='predicted')
+    plt.title('Linear fitting')
+    plt.legend()
+    plt.show()
+
+
+def evaluate_model_parameters(X, t):
+    w = np.linalg.inv(X.T @ X) @ X.T @ t
+    print(w)
+    return w
+
+
+data = read_olympics_data()
+X = np.array(data[:, 0], dtype='float').reshape(-1, 1)
+X = np.hstack((np.ones_like(X), X))
+t = np.array(data[:, 1], dtype='float').reshape(-1, 1)
+print(X.shape, t.shape)
+w = evaluate_model_parameters(X, t)
+Xnew = np.array(np.linspace(X[0, 1], X[-1, 1])).reshape(-1, 1)  # columnize
+Xnew = np.hstack((np.ones_like(Xnew), Xnew))
+tnew = np.dot(Xnew, w)
+plot_data(X[:, 1], t, Xnew[:, 1], tnew)
+
+
+
